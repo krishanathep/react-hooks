9802fb1b39c55c32bfd6b9388a2aea63498d9390
Krishanathep Jareanpong <krishanathep@gmail.com>
2021-03-30 09:26:23 +0700

    Update Count App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+
+function WelcomeMessage(){
+  return <p>Welcome to react hooks!</p>
+}
 
 const App = () => {
   const [count, setCount] = useState(0);
 
+  useEffect(()=>{
+    console.log(`You clicked ${count} times`)
+  }, [count])
+
   return (
     <div align="center">
+      <WelcomeMessage/>
       <p>You clicked {count} times</p>
       <button onClick={() => setCount(count + 1)}>+</button>{" "}
       <button onClick={() => setCount(count - 1)}>-</button>{" "}
