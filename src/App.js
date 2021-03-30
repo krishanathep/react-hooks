113ab3e87004741cf66e7a3c93fbff1ec5ecb558
Krishanathep Jareanpong <krishanathep@gmail.com>
2021-03-30 08:37:49 +0700

    Create Count App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import Hooks from './pages/Hooks'
+import React, { useState } from "react";
 
-function App() {
+const App = () => {
+  const [count, setCount] = useState(0);
+
   return (
-    <div className="App">
-      <Hooks/>
+    <div align="center">
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>+</button>{" "}
+      <button onClick={() => setCount(count - 1)}>-</button>{" "}
+      <button onClick={() => setCount(0)}>Reset</button>
     </div>
   );
-}
+};
 
 export default App;
