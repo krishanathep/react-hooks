97ee96bae6283c3ea290d2b1e54269fb3c74f98a
Krishanathep Jareanpong <krishanathep@gmail.com>
2021-04-06 09:57:50 +0700

    include loading

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,30 +1,67 @@
 import React, { useState, useEffect } from "react";
-import { Container, CardDeck, Card, Row, Button } from "react-bootstrap";
+import { Container, CardDeck, Card, Row, Button, Spinner } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { useParams, useHistory } from "react-router-dom";
-import { format } from 'date-fns'
+import { format } from "date-fns";
 import axios from "axios";
 
 const Detail = () => {
   const { id, title } = useParams();
-  const history = useHistory()
+  const history = useHistory();
 
   const [details, setDetails] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   const getData = async (id) => {
-    const res = await axios.get("https://api.codingthailand.com/api/course/"+ id,)
-    setDetails(res.data.data);
+    try {
+      setLoading(true);
+      const res = await axios.get(
+        "https://api.codingthailand.com/api/course/" + id
+      );
+      setDetails(res.data.data);
+    } catch (error) {
+      console.log(error);
+      setError(error);
+    } finally {
+      setLoading(false);
+    }
   };
 
   useEffect(() => {
     getData(id);
-  },[id]);
+  }, [id]);
+
+  if (loading === true) {
+    return (
+      <div className="text-center mt-3">
+        <Spinner animation="border" variant="primary" />
+      </div>
+    );
+  }
+
+  if (error) {
+    return (
+      <div className="text-center mt-3">
+        <p>เกิดข้อผิดพลาดจาก Server กรุณาลองใหม่</p>
+        <p>{error.response.data.message}</p>
+      </div>
+    );
+  }
 
   return (
-    <div className='mt-3 my-3'>
+    <div className="mt-3 my-3">
       <Container>
-        <Button onClick={()=>{history.goBack()}}>Go Back</Button>
-        <h2>{title} - {id}</h2>
+        <Button
+          onClick={() => {
+            history.goBack();
+          }}
+        >
+          Go Back
+        </Button>
+        <h2>
+          {title} - {id}
+        </h2>
         <Row>
           <CardDeck>
             {details.map((detail, index) => {
@@ -33,7 +70,9 @@ const Detail = () => {
                   <Card>
                     <Card.Body>
                       <Card.Title>{detail.ch_title}</Card.Title>
-                      <Card.Text>{format(new Date(detail.ch_dateadd), 'dd-MMM-yyyy')}</Card.Text>
+                      <Card.Text>
+                        {format(new Date(detail.ch_dateadd), "dd-MMM-yyyy")}
+                      </Card.Text>
                     </Card.Body>
                   </Card>
                 </div>
--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,56 +1,101 @@
-import React, { useState, useEffect } from 'react'
-import { Container, Table, Image, Badge, Button } from "react-bootstrap";
+import React, { useState, useEffect } from "react";
+import {
+  Container,
+  Table,
+  Image,
+  Badge,
+  Button,
+  Spinner,
+} from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { format } from 'date-fns'
-import { Link } from 'react-router-dom'
-import axios from 'axios'
+import { format } from "date-fns";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
 const Products = () => {
   const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   const getData = async () => {
-    const res = await axios.get('https://api.codingthailand.com/api/course')
-    //console.log(res.data)
-    setProducts(res.data.data)
+    try {
+      setLoading(true);
+      const res = await axios.get("https://api.codingthailand.com/api/course");
+      //console.log(res.data)
+      setProducts(res.data.data);
+    } catch (error) {
+      console.log(error);
+      setError(error);
+    } finally {
+      setLoading(false);
+    }
   };
 
-  useEffect(()=>{
-    getData()
-  })
+  useEffect(() => {
+    getData();
+  }, []);
+
+  //set loading
+  if (loading === true) {
+    return (
+      <div className="text-center mt-3">
+        <Spinner animation="border" variant="primary" />
+      </div>
+    );
+  }
+
+  //set error
+  if (error) {
+    return (
+      <div className="text-center mt-3">
+        <p>เกิดข้อผิดพลาดจาก Server กรุณาลองใหม่</p>
+        <p>{error.response.data.message}</p>
+      </div>
+    );
+  }
 
   return (
-    <div className='mt-3 my-3'>
+    <div className="mt-3 my-3">
       <Container>
         <h2>Products</h2>
         <Table bordered hover>
           <thead>
-          <tr>
-            <th>ID</th>
-            <th>Name</th>
-            <th>Detail</th>
-            <th>Create At</th>
-            <th>View</th>
-            <th>Picture</th>
-            <th>Actions</th>
-          </tr>
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Detail</th>
+              <th>Create At</th>
+              <th>View</th>
+              <th>Picture</th>
+              <th>Actions</th>
+            </tr>
           </thead>
           <tbody>
-          {products.map((product, index)=>(
-            <tr key={product.id}>
-            <td>{product.id}</td>
-            <td>{product.title}</td>
-            <td>{product.detail}</td>
-            <td>
-              {format(new Date(product.date), 'dd-MMM-yyyy')}
-            </td>
-            <td><Badge pill variant='info'>{product.view}</Badge></td>
-            <td><Image src={product.picture} thumbnail width={100} /></td>
-            <td>
-              <Link to={`/detail/${product.id}/title/${product.title}`} className='btn btn-primary'>View</Link>{' '}
-              <Button variant='success'>Add to Cart</Button>
-            </td>
-          </tr>
-          ))}
+            {products.map((product, index) => (
+              <tr key={product.id}>
+                <td>{product.id}</td>
+                <td>{product.title}</td>
+                <td>{product.detail}</td>
+                <td>{format(new Date(product.date), "dd-MMM-yyyy")}</td>
+                <td>
+                  <Badge pill variant="info">
+                    {product.view}
+                  </Badge>
+                </td>
+                <td>
+                  <Image src={product.picture} thumbnail width={100} />
+                </td>
+                <td>
+                  <Link
+                    to={`/detail/${product.id}/title/${product.title}`}
+                    className="btn btn-primary"
+                  >
+                    View
+                  </Link>{" "}
+                  <Button variant="success">Add to Cart</Button>
+                </td>
+              </tr>
+            ))}
           </tbody>
         </Table>
       </Container>
